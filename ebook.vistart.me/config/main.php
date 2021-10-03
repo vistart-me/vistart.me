8a6db94d99b8aa1b5966291930be1c0be73a5265
vistart <i@vistart.me>
2021-10-03 22:44:12 +0800

    引入 `ebook.vistart.me` 模板。

--- ebook.vistart.me/config/main.php
+++ ebook.vistart.me/config/main.php
@@ -6,21 +6,21 @@ $params = array_merge(
     require __DIR__ . '/params-local.php'
 );
 return [
-    'id' => '_vistart_me',
+    'id' => 'ebook_vistart_me',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
-    'controllerNamespace' => '_vistart_me\controllers',
+    'controllerNamespace' => 'ebook_vistart_me\controllers',
     'components' => [
         'request' => [
-            'csrfParam' => '_csrf_vistart_me',
+            'csrfParam' => '_csrf_ebook_vistart_me',
         ],
         'user' => [
             'identityClass' => 'common\model\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity_vistart_me', 'httpOnly' => true],
+            'identityCookie' => ['name' => '_identity_ebook_vistart_me', 'httpOnly' => true],
         ],
         'session' => [
-            'name' => '_session_vistart_me'
+            'name' => '_session_ebook_vistart_me'
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
